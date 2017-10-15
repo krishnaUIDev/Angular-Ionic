e9135c12a8251451448fd9068ecf99a037281366
krishnaUIDev <krishna.kondoju007@gmail.com>
2017-10-15 14:11:10 -0500

    new page added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,24 +7,7 @@ import { MainPage } from '../main/main';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
-  /*animations: [
 
-    trigger('bounce', [
-      state('*', style({
-        transform: 'translateX(0)'
-      })),
-      transition('* => rightSwipe', animate('700ms ease-out', keyframes([
-        style({transform: 'translateX(0)', offset: 0}),
-        style({transform: 'translateX(-65px)',  offset: 0.3}),
-        style({transform: 'translateX(0)',     offset: 1.0})
-      ]))),
-      transition('* => leftSwipe', animate('700ms ease-out', keyframes([
-        style({transform: 'translateX(0)', offset: 0}),
-        style({transform: 'translateX(65px)',  offset: 0.3}),
-        style({transform: 'translateX(0)',     offset: 1.0})
-      ])))
-    ])
-  ]*/
 })
 export class HomePage {
   @ViewChild(Slides) slides: Slides;
@@ -39,16 +22,8 @@ export class HomePage {
   }
   slideChanged(){
     if(this.slides.isEnd()){
-      this.skipMsg = "Alright i Got It";
+      this.skipMsg = "Alright, I Got It";
     }
   }
-  /*slideMoved(){
-    if(this.slides.getActiveIndex() >= this.slides.getPreviousIndex())
-      this.state = 'rightSwipe';
-    else
-      this.state = 'leftSwipe';
-  }
-  animation(){
-    this.state = 'x';
-  }*/
+
 }
