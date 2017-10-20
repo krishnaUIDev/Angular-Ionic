cbf3ce1796fb6a9299bad1d860143d2da0e47404
krishnaUIDev <krishna.kondoju007@gmail.com>
2017-10-20 00:56:19 -0500

    add

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 
-
 @IonicPage()
 @Component({
   selector: 'page-menu',
   templateUrl: 'menu.html',
 })
 export class MenuPage {
-  imageURL
+  imageURL;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
